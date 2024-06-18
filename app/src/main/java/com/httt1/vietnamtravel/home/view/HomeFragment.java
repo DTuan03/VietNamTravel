afd16f290e952e787774b74ff3fa659dc0cb2d38
Đặng Anh Tuấn <137882845+DTuan03@users.noreply.github.com>
2024-06-19 00:42:12 +0700

    upate homefragment

--- app/src/main/java/com/httt1/vietnamtravel/home/view/HomeFragment.java
+++ app/src/main/java/com/httt1/vietnamtravel/home/view/HomeFragment.java
@@ -4,41 +4,24 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.httt1.vietnamtravel.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     public HomeFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
@@ -47,7 +30,6 @@ public class HomeFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,11 +38,16 @@ public class HomeFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Bundle bundle = getArguments();
+        if (bundle != null){
+            String userId = bundle.getString("UserId");
+//            Log.d("HomeFragmnet", userId);
+        }
+        return view;
     }
 }
--- app/src/main/java/com/httt1/vietnamtravel/main/view/MainActivity.java
+++ app/src/main/java/com/httt1/vietnamtravel/main/view/MainActivity.java
@@ -1,17 +1,32 @@
 package com.httt1.vietnamtravel.main.view;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.httt1.vietnamtravel.R;
+import com.httt1.vietnamtravel.home.view.HomeFragment;
+import com.httt1.vietnamtravel.main.presenter.MainContract;
+import com.httt1.vietnamtravel.main.presenter.MainPresenter;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements MainContract.View {
+    Fragment fragmentHome = new HomeFragment();
+    private BottomNavigationView bottomNavigationView;
+    private MainContract.Presenter presenter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +37,45 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        init();
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.fragment_home_status_bar));
+        Fragment homefragment = new HomeFragment();
+        loadFragment(homefragment);
+        presenter = new MainPresenter(this, this);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                return presenter.onClickItem(menuItem);
+            }
+        });
+    }
+
+    private void init(){
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.activity_main_bnv);
+    }
+
+    @Override
+    public void loadFragment(Fragment fragment, int setStatusBarColor) {
+        loadFragment(fragment);
+        getWindow().setStatusBarColor(setStatusBarColor);
+    }
+
+    private void loadFragment(Fragment fragment){
+        getIntentData(fragment);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.activity_main_fragment_container, fragment);
+        transaction.addToBackStack(null); // theem fragment vao ngan xep backstack de khi an quay lai no se quay lai
+        transaction.commit();
+    }
+    private void getIntentData(Fragment fragment){
+        //nhan du lieu
+        Intent intent = getIntent();
+        if(intent != null){
+            String valueUserId = intent.getStringExtra("UserId");
+            //truyen du lieu den fragment
+            Bundle bundle = new Bundle();
+            bundle.putString("UserId", valueUserId);
+            fragment.setArguments(bundle);
+        }
     }
 }
