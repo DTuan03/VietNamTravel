fa91aaca641c931738959c030ad2b1375cd794f0
Đặng Anh Tuấn <137882845+DTuan03@users.noreply.github.com>
2024-06-19 22:19:57 +0700

    update HomeRepository

--- app/src/main/java/com/httt1/vietnamtravel/home/model/HomeRepository.java
+++ app/src/main/java/com/httt1/vietnamtravel/home/model/HomeRepository.java
@@ -30,41 +30,28 @@ public class HomeRepository {
         executorService.execute(new Runnable() {
             @Override
             public void run() {
-                String queryTour = "SELECT TourId, NameTour, PriceTour FROM Tour WHERE TypeTour = ? ";
-                String queryImgTour = "SELECT TourId, ImgResource FROM ImgTour WHERE ImgPosition = 1";
+                String query = "SELECT Tour.TourId, Tour.NameTour, Tour.PriceTour, ImgTour.ImgResource " +
+                        "FROM Tour " +
+                        "INNER JOIN ImgTour ON Tour.TourId = ImgTour.TourId " +
+                        "WHERE Tour.TypeTour = ? " + "AND ImgTour.ImgPosition = 1";
+
                 try(
                         Connection connection = sqlServerDataSource.getConnection();
-                        PreparedStatement statementTour = connection.prepareStatement(queryTour);
-                        PreparedStatement statementImgTour = connection.prepareStatement(queryImgTour);
+                        PreparedStatement statement = connection.prepareStatement(query);
                 )
                 {
-                    statementTour.setString(1, typeTour);
-                    ResultSet resultSetTour = statementTour.executeQuery();
-                    ResultSet resultSetImgTour = statementImgTour.executeQuery();
-                    //Phai duyet nhu nay neu kh se chi tra về 1 kết quả, và thư viem jtdc chỉ cho phép duyet 1 lan nen kh dung duoc resultSetImgTour.beforeFirst(); // Đặt con trỏ về đầu ResultSet để duyệt lại từ đầu
-                    //nen truoc khi do minh phai luu anh vao 1 danh sach roi duyet danh scah do
-                    List<String> imgResources = new ArrayList<>();
-                    while (resultSetImgTour.next()) {
-                        String tourId = resultSetImgTour.getString("TourId");
-                        String imgResource = resultSetImgTour.getString("ImgResource");
-                        imgResources.add(tourId + ";" + imgResource); // Lưu theo định dạng TourId|ImgResource
-                    }
+                    statement.setString(1, typeTour);
 
+                    ResultSet resultSet = statement.executeQuery();
                     // Tạo danh sách để chứa kết quả
                     List<HomeModel> tours = new ArrayList<>();
-                    while (resultSetTour.next()) {
-                        String tourId = resultSetTour.getString("TourId");
-                        String nameTour = resultSetTour.getString("NameTour");
-                        int priceTour = resultSetTour.getInt("PriceTour");
-                        // Kiểm tra và thêm dữ liệu từ danh sách imgResources vào tours
-                        for (String imgResource : imgResources) {
-                            String[] parts = imgResource.split(";");
-                            if (tourId.equals(parts[0])) {
-                                HomeModel tour = new HomeModel(parts[1], nameTour, priceTour);
-                                tours.add(tour);
-                                break; // Thoát vòng lặp khi đã tìm được ảnh cho TourId này
-                            }
-                        }
+                    while (resultSet.next()) {
+                        String tourId = resultSet.getString("TourId");
+                        String nameTour = resultSet.getString("NameTour");
+                        int priceTour = resultSet.getInt("PriceTour");
+                        String imgUrl = resultSet.getString("ImgResource");
+                        HomeModel tour = new HomeModel(imgUrl, nameTour, priceTour);
+                        tours.add(tour);
                     }
                     comboCallBack.listCombo(tours);
                     Log.d("So luong", "So luong " + tours.size());
